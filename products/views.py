071d34353df832812abce9dda70e273521d61f87
Emilakesofficial <157388465+Emilakesofficial@users.noreply.github.com>
2025-06-29 18:08:21 +0100

    fixed 404 error

--- products/views.py
+++ products/views.py
@@ -9,24 +9,25 @@ from rest_framework.permissions import IsAuthenticated
 
 
 class CustomUnitView(APIView):
-        def get(self, request):
-            try:
-                unit = CustomUnit.objects.all()
-                serializer = CustomUnitSerializer(unit, many=True)
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        try:
+            unit = CustomUnit.objects.all()
+            serializer = CustomUnitSerializer(unit, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error':str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    def post(self, request):
+        try:
+            serializer = CustomUnitSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
-            except Exception as e:
-                return Response({'error':str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            
-        def post(self, request):
-            try:
-                serializer = CustomUnitSerializer(data=request.data)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data, status=status.HTTP_200_OK)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            except Exception as e:
-                return Response({'error':str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error':str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
 class CustomUnitDetailedView(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request, id):
