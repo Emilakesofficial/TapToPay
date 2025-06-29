f4f6386d587c7d8f209a7ebab9f00d444a24efaa
Emilakesofficial <157388465+Emilakesofficial@users.noreply.github.com>
2025-06-29 17:37:44 +0100

    fixed 404 error

--- onbaording/settings.py
+++ onbaording/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['paypos.on render.com']
+ALLOWED_HOSTS = ['paypos.onrender.com']
 
 
 # Application definition
